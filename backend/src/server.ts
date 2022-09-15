a934372556649c6d9c899d26ef77ce2d0eb6cfa4
Tomáš Filo <tfilosk@gmail.com>
2022-09-15 15:47:14 +0200

    Changed logo and removed mock user for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,21 +7,21 @@ import sequelize from './util/database';
 const port = process.env.PORT || 3000;
 
 (async () => {
-    await sequelize.sync({
-        alter: true,
-    });
+    await sequelize.sync();
 
-    await User.findOrCreate({
-        where: {
-            username: 'test',
-        },
-        defaults: {
-            username: 'test',
-            password: 'Test1234',
-            email: 'email@email.email',
-            confirmed: true
-        }
-    })
+    if (process.env.NODE_ENV === 'development') {
+        await User.findOrCreate({
+            where: {
+                username: 'test',
+            },
+            defaults: {
+                username: 'test',
+                password: 'Test1234',
+                email: 'email@email.email',
+                confirmed: true,
+            },
+        });
+    }
 
     app.listen(port, () => console.info(`Server running on port ${port}`));
 })();
